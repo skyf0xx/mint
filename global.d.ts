1ff377513bda61c9bd29e94287d06f7b8fc2cf42
zoka-agari <zokaagari@proton.me>
2025-01-23 13:52:26 +1100

    chore: add type

---
+++ global.d.ts
@@ -0,0 +1,8 @@
+// types/global.d.ts
+interface Window {
+    arweaveWallet?: {
+        connect(permissions: string[]): Promise<void>;
+        disconnect(): Promise<void>;
+        getActiveAddress(): Promise<string>;
+    };
+}
