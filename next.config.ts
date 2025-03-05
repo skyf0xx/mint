16b6ebd695aedafc34990fd5bd3ae3b10c2e77e2
zoka-agari <zokaagari@proton.me>
2025-03-05 20:48:33 +1100

    feat: simplify routing

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProduction = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-    assetPrefix: isProduction
-        ? 'https://mithril-mint-token.ar.io'
-        : 'http://localhost:3000',
+    assetPrefix: './',
     output: 'export',
-    trailingSlash: true,
     basePath: '',
     images: {
         unoptimized: true,
